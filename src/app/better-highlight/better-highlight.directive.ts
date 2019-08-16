d72abcc680a6e0e49d5057b32044a5f01c4933f0
“Maxaplays” <rgmateo@hotmail.com>
2019-08-16 15:23:57 -0500

    CLOUDTAXIS-251 Directives Deep Dive

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'green';
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.highlightColor;
+  }
+}
